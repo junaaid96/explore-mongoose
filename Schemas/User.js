878b5e78f5c885606db7110a04914a9e60657bd5
junaaid96 <info.junaidulislam@gmail.com>
2023-03-11 12:45:43 +0600

    minor changes

--- Schemas/User.js
+++ Schemas/User.js
@@ -3,9 +3,18 @@ const addressSchema = require("./Address.js");
 
 const userSchema = new mongoose.Schema({
     name: String,
-    age: { type: Number, min: 1 },
-    email: { type: String, required: true, lowercase: true },
-    createdAt: { type: Date, immulatable: true, default: () => Date.now() },
+    age: {
+        type: Number,
+        // min: 1,
+        // max:100,
+        validate: {
+            validator: (v) => v > 0,
+            message: (props) =>
+                `You entered ${props.value}. Age cannot be a negative number!`,
+        },
+    },
+    email: { type: String, required: true, lowercase: true, minLength: 5 },
+    createdAt: { type: Date, immutable: true, default: () => Date.now() },
     updatedAt: { type: Date, immutable: true, default: () => Date.now() },
     bestFriend: mongoose.SchemaTypes.ObjectId,
     hobbies: [String],
